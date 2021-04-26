b87a588ab2ccdc7faa933e6da30636b3f900a3d0
Hélène <helen-r@hotmail.fr>
2021-04-26 14:22:08 +0200

    tuto_youtube_symfony

---
+++ demo/src/Controller/BlogController.php
@@ -0,0 +1,123 @@
+<?php
+
+//http://127.0.0.1:8000/
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Entity\Comment;
+use App\Form\ArticleType;
+use App\Form\CommentType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticleRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\BrowserKit\Request as BrowserKitRequest;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
+use Symfony\Component\HttpFoundation\Request;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index(ArticleRepository $repo): Response
+    {
+        $articles = $repo->findAll();
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'articles' => $articles
+        ]);
+    }
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(){
+        return $this->render('blog/home.html.twig', [
+            'title' => "Bienvenue prout",
+            'age' => 16
+        ]);
+    }
+
+    /**
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
+     */
+    public function form(Article $article = null, Request $request, EntityManagerInterface $manager){
+
+        if(!$article){
+            $article = new Article();
+        }
+
+        /*
+        $form = $this->createFormBuilder($article)
+                     ->add('title', TextType::class)
+                     ->add('content', TextareaType::class, [
+                         'attr' => [
+                             'placeholder' => "Contenu de l'article",
+                             'class' => 'form-control'
+                         ]
+                     ])
+                     ->add('image', TextType::class, [
+                         'attr' => [
+                             'placeholder' => "Image de l'article",
+                             'class' => 'form-control'
+                         ]
+                     ])
+                     ->getForm();
+        */
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if(!$article->getId()){
+            $article->setCreatedAt(new \DateTime());
+        }
+
+        if($form->isSubmitted() && $form->isValid()){
+            $article->setCreatedAt(new \DateTime());
+
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('blog/create.html.twig',[
+            'formArticle' => $form->createView(),
+            'editMode' => $article->getId() !== null
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     */
+    public function show(Article $article, Request $request, EntityManagerInterface $manager){
+        $comment = new Comment;
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $comment->setCreateAt(new \DateTime())
+                    ->setArticle($article);
+            $manager->persist($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show',['id'=> $article->getId()]);
+        }
+
+        return $this->render('blog/show.html.twig', [
+            'article'=> $article,
+            'commentForm' => $form->createView()
+        ]);
+    }
+    
+
+}
---
+++ demo/src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+
+        //créer 3 catégories fakes
+
+        for($i = 1;$i <= 3; $i++){
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            //créer entre 4 et 6 articles
+            for($j = 1; $j <= mt_rand(4, 6) ; $j++){
+                $article = new Article();
+
+                $content ='<p>'.join('</p><p>', $faker->paragraphs(5)) . '</p>';
+
+                $article->setTitle($faker->sentence())
+                        ->setContent($content)
+                        ->setImage($faker->imageUrl())
+                        ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                        ->setCategory($category);
+
+                $manager->persist($article);
+
+                for($k = 1; $k <= mt_rand(4, 10); $k++){
+                    $comment = new Comment();
+
+                    $content ='<p>'.join('</p><p>',$faker->paragraphs(2)) . '</p>';
+
+                    $days = (new \DateTime())->diff($article->getCreatedAt())->days;
+
+                    $comment->setAuthor($faker->name)
+                            ->setContent($content)
+                            ->setCreateAt($faker->dateTimeBetween('-' . $days . ' days'))
+                            ->setArticle($article);
+
+                    $manager->persist($comment);
+                }
+            }
+        }
+
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
